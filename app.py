08a084974614bb946b43f0e87a0f25687d929c23
Peeyush Gupta <peeyushgupta91@gmail.com>
2021-03-15 13:29:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,6 @@
-import http.server
-import socketserver
+from flask import Flask
+app = Flask(__name__)
 
-PORT = 8101
-
-handler = http.server.SimpleHTTPRequestHandler
-
-with socketserver.TCPServer(("", PORT), handler) as httpd:
-    print("Server started at localhost:" + str(PORT))
-    httpd.serve_forever()
-
+@app.route("/")
+def hello():
+    return "Hello, World!"
