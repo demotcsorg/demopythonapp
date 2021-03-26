02eaed97b0ece3d0c1cfca1a8988ad0867befcb4
Peeyush Gupta <peeyushgupta91@gmail.com>
2021-03-26 20:45:50 +0530

    Adding more details

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import sqlite3
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for, flash, redirect
 
 def get_db_connection():
     conn = sqlite3.connect('database.db')
@@ -7,6 +7,7 @@ def get_db_connection():
     return conn
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'samplekey'
 
 @app.route('/')
 def index():
@@ -14,3 +15,22 @@ def index():
     posts = conn.execute('SELECT * FROM hospital').fetchall()
     conn.close()
     return render_template('index.html', posts=posts)
+
+
+@app.route('/add', methods=('GET', 'POST'))
+def add():
+    if request.method == 'POST':
+         name = request.form['name']
+         address = request.form['address']
+         contact = request.form['contact']
+         conn = get_db_connection()
+         conn.execute('INSERT INTO hospital (name, address, phone) VALUES (?, ?, ?)', (name, address, contact))
+         conn.commit()
+         conn.close()
+         return redirect(url_for('index'))
+
+    return render_template('add.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
