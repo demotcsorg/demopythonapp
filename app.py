fa3a8ecc567590b42326c29685205422dcbf259b
Peeyush Gupta <peeyushgupta91@gmail.com>
2021-03-30 18:24:51 +0530

    Adding timeout for sqlite

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import sqlite3
 from flask import Flask, render_template, request, url_for, flash, redirect
 
 def get_db_connection():
-    conn = sqlite3.connect('database.db')
+    conn = sqlite3.connect('database.db', timeout=20)
     conn.row_factory = sqlite3.Row
     return conn
 
